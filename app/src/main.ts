edf90e365d8ba1d7e0704b82a0f330928723ac69
zherunliu <1437257281@qq.com>
2026-02-26 23:59:53 +0800

    chore: add pkgs & daisyui

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import "./style.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
